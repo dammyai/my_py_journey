c819a072d833370a886b289330e33a04049b34d0
Dammy Arinde <65983923+dammyai@users.noreply.github.com>
2020-06-18 22:48:55 -0400

    Add files via upload

---
+++ checksyspy.py
@@ -0,0 +1,126 @@
+"""import sys
+print('Python: {}'.format(sys.version))
+# scipy
+import scipy
+print('scipy: {}'.format(scipy.__version__))
+# numpy
+import numpy
+print('numpy: {}'.format(numpy.__version__))
+# matplotlib
+import matplotlib
+print('matplotlib: {}'.format(matplotlib.__version__))
+# pandas
+import pandas
+print('pandas: {}'.format(pandas.__version__))
+# scikit-learn
+import sklearn
+print('sklearn: {}'.format(sklearn.__version__))
+
+# /Users/oluwadamilolaarinde/opt/anaconda3/bin/idle3.7
+
+
+# 1.	Installing the Python and SciPy platform.
+# 2.	Loading the dataset.
+# 3.	Summarizing the dataset.
+# 4.	Visualizing the dataset.
+# 5.	Evaluating some algorithms.
+# 6.	Making some predictions.
+"""
+
+# load libraries
+
+from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# 2) loadind dataset
+# use panda to load dataset iris flowers from UCI Machine Learning Repository
+
+url = "https://raw.githubusercontent.com/jbrownlee/Datasets/master/iris.csv"
+names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+dataset = read_csv(url, names=names)
+
+# 3) summarizing dataset
+# data dimensions - e.g to see the num of instances and attributes of our dataset
+print(dataset.shape)  # prints(rows,column)
+print(dataset.head(20)) # see the first 20 rows
+print(dataset.describe())# see attribute descriptions like mean, count, std, min, max
+print(dataset.groupby('class').size()) # class distribution of the dataset
+
+
+# 4) visualizing dataset
+
+# univariate plots
+# box and whisker plots
+dataset.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
+pyplot.show()
+
+# histogram to get an idea of the distribution
+
+# histograms
+dataset.hist()
+pyplot.show()
+
+# multivariate plots
+# scatter plot matrix
+scatter_matrix(dataset)
+pyplot.show()
+
+# 5) evaluating algo
+# Split-out validation dataset
+array = dataset.values
+X = array[:,0:4]
+y = array[:,4]
+X_train, X_validation, Y_train, Y_validation = train_test_split(X, y, test_size=0.20, random_state=1)
+
+
+# Spot Check Algorithms
+models = []
+models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
+models.append(('LDA', LinearDiscriminantAnalysis()))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('CART', DecisionTreeClassifier()))
+models.append(('NB', GaussianNB()))
+models.append(('SVM', SVC(gamma='auto')))
+# evaluate each model in turn
+results = []
+names = []
+for name, model in models:
+	kfold = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
+	cv_results = cross_val_score(model, X_train, Y_train, cv=kfold, scoring='accuracy')
+	results.append(cv_results)
+	names.append(name)
+	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
+
+
+
+# Compare Algorithms
+pyplot.boxplot(results, labels=names)
+pyplot.title('Algorithm Comparison')
+pyplot.show()
+
+
+
+# 6) Make predictions on validation dataset
+model = SVC(gamma='auto')
+model.fit(X_train, Y_train)
+predictions = model.predict(X_validation)
+
+
+
+# Evaluate predictions
+print(accuracy_score(Y_validation, predictions))
+print(confusion_matrix(Y_validation, predictions))
+print(classification_report(Y_validation, predictions))
